86771faba15f717ce82e9f7e888e3e4aa4e5b705
David Larronda <david.larronda@gmail.com>
2024-11-20 13:18:58 -0300

    comitando

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Codaciencia</h1>
+        <button>Clique aqui</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
